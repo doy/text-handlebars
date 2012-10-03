d28fe9e9d3799aa5eb32801d8fa511e2bbdc5c6f
Jesse Luehrs <doy@tozt.net>
2012-10-03 18:36:06 -0500

    remove debugging code

--- lib/Text/Handlebars/Compiler.pm
+++ lib/Text/Handlebars/Compiler.pm
@@ -31,21 +31,6 @@ sub _generate_include {
     return $self->SUPER::_generate_include($node);
 }
 
-if (0) {
-    our $_recursing;
-    around compile_ast => sub {
-        my $orig = shift;
-        my $self = shift;
-
-        my @ast = do {
-            local $_recursing = 1;
-            $self->$orig(@_);
-        };
-        use Data::Dump; ddx(\@ast) unless $_recursing;
-        return @ast;
-    };
-}
-
 __PACKAGE__->meta->make_immutable;
 no Any::Moose;
 
