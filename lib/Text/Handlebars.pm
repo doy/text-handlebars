01c2301937d6c0029408a40fa5e4a5c868516e4c
Jesse Luehrs <doy@tozt.net>
2012-10-15 00:42:07 -0500

    turned render_string into an opcode

--- lib/Text/Handlebars.pm
+++ lib/Text/Handlebars.pm
@@ -164,10 +164,6 @@ sub _register_builtin_methods {
     my ($funcs) = @_;
 
     weaken(my $weakself = $self);
-    $funcs->{'(render_string)'} = sub {
-        my ($to_render, $vars) = @_;
-        return $weakself->render_string($to_render, $vars);
-    };
     $funcs->{'(find_file)'} = sub {
         my ($filename) = @_;
         return $filename if try { $weakself->find_file($filename); 1 };
